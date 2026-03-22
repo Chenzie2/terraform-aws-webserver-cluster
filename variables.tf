045d30522a76480f806e33ac1b251bd13ee9ee9f
g_zawie <gzawie24@gmail.com>
2026-03-22 15:25:40 +0300

    Add enable_autoscaling variable

--- variables.tf
+++ variables.tf
@@ -30,3 +30,9 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "enable_autoscaling" {
+  description = "Whether to enable autoscaling policies"
+  type        = bool
+  default     = true
+}
